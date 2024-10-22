073816118484a59f1918e3b20e9a8afcdf28a6f4
Sam Ezebunandu <sam.ezebunandu@gmail.com>
2024-10-21 21:57:46 -0600

    WIP: serving with Handle; struct with counter

---
+++ Chapter16/Activity16.01/go.mod
@@ -0,0 +1,3 @@
+module Activity16.01
+
+go 1.23.2
---
+++ Chapter16/Activity16.01/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+    "log"
+)
+
+type PageWithCounter struct {
+    counter int
+    content string
+    heading string
+}
+
+func (p PageWithCounter) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+    p.counter++
+    msg := fmt.Sprintf("Header: %s, Content: %s, page views: %d", p.content, p.heading, p.counter)
+    w.Write([]byte(msg))
+}
+
+func main() {
+    hello := PageWithCounter{heading: "hello world", content: "This is the main page"}
+    chapter1 := PageWithCounter{heading: "Chapter 1", content: "This is the first chapter"}
+    chapter2 := PageWithCounter{heading: "Chapter 2", content: "This is the second chapter"}
+    
+    http.Handle("/", &hello)
+    http.Handle("/chapter1", &chapter1)
+    http.Handle("/chapter2", &chapter2)
+    log.Fatal(http.ListenAndServe(":8080", nil))
+}
