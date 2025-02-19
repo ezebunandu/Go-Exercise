a174538fbd05da306fec5a0b4b5f357008563453
Sam Ezebunandu <sam.ezebunandu@gmail.com>
2025-02-19 06:40:23 -0700

    how not to sync goroutines with sleep

---
+++ Chapter18/Exercise18.01/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+    "log"
+    "time"
+) 
+
+func sum(from, to int) int {
+    res := 0
+    for i := from; i <= to; i++ {
+        res += i
+    }
+    return res
+}
+
+func main() {
+    var s1, s2 int
+    go func ()  {
+        s1 = sum(1, 100)
+    }()
+    s2 = sum(1, 10)
+    time.Sleep(2 * time.Second)
+    log.Println(s1, s2)
+}
